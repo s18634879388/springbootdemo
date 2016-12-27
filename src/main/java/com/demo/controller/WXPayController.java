6ec093a8c1231a3e269689706264cc922ad33b18
史小绮 <shixiaoqi@9hcom.com>
2016-12-27 15:38:49 +0800

    wxpay

--- src/main/java/com/demo/controller/WXPayController.java
+++ src/main/java/com/demo/controller/WXPayController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by Administrator on 2016/12/23.
@@ -14,8 +15,20 @@ public class WXPayController {
         HashMap<String,String> paramMap = new HashMap<>();
         paramMap.put("appid","xxxxxxxx");
         paramMap.put("mch_id","1111111111");
-//        paramMap.put()
-//  http://javatech.wang/index.php/archives/84/
+        paramMap.put("nonce_str", String.valueOf(new Random(10).nextInt()));
+        paramMap.put("body","ipad");
+        paramMap.put("out_trade_no", String.valueOf(new Random(10).nextInt()));
+        paramMap.put("total_fee", 1000+"");
+        paramMap.put("spbill_create_ip", "127.0.0.1");
+//        paramMap.put("notify_url", this.notifyUrl);
+        paramMap.put("trade_type", "NATIVE"); //交易类型
+        paramMap.put("product_id", "100001");
+        //根据微信签名规则，生成签名。随机参数可以在商户后台管理系统中进行设置。
+//        paramMap.put("sign", PaymentUtil.getSignature(paramMap, "beGPax3F1EtxxxxxxofcerMRqNvt9XJ2"));
+
+
+
+//  http://javatech.wang/index.php/archives/84/   http://blog.csdn.net/wangqiuyun/article/details/51241064
 
         return null;
     }
